957568fe421a80440a0c0dc1974371929d394f25
Arvin Foroutan <arvin.foroutan@gmail.com>
2014-02-08 11:05:35 -0800

    RESTful controller pertaining to the Users model

---
+++ app/controllers/UserController.php
@@ -0,0 +1,67 @@
+<?php
+
+class UserController extends \BaseController {
+
+	public function __construct()
+	{
+		$this->beforeFilter('auth');
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$response = array('data' => User::all()->toArray());
+
+    	return Response::make($response, 200);
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		$response = array('data' => User::findOrFail($id)->toArray());
+
+    	return Response::make($response, 200);
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
