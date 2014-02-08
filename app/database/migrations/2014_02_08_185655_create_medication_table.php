c854f425b23d390170e8691a3e84b4eddffec813
WebDevGirl <thedpq@gmail.com>
2014-02-08 11:28:41 -0800

    Create base tables for medications, appointments, and contacts.
    Init

---
+++ app/database/migrations/2014_02_08_185655_create_medication_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicationTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		 Schema::create('medications', function($table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+     	    $table->string('dosage');
+            $table->string('requirements');
+            $table->string('notes');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
---
+++ app/database/migrations/2014_02_08_190518_create_appointments_tables.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('appointments', function($table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+           	$table->string('name');
+           	$table->string('location');
+           	$table->string('appointment_datetime');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
