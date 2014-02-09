655c6d6ad4e4d391c991a1daad149e68f5e37cee
WebDevGirl <thedpq@gmail.com>
2014-02-08 19:02:40 -0800

    Call user store() function via API to create new user if contact doesn't exist.

--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -50,11 +50,19 @@ class ContactController extends \BaseController {
         } 
 
         /* See if user is already in the system */
-        $contact_data = User::where("email",$input['email'])->first()->toArray();
+        $contact_data = User::where("email",$input['email'])->first();
 
         /* Add New User if they don't exist */
         if (!$contact_data) {
-        	// To Do contact_data = new User()
+        	/* Consume own API to create user */ 
+
+        	$request = Request::create('/api/v1/users', 'POST', $input);
+			$response = json_decode(Route::dispatch($request)->getContent(),true);
+			if ($response['status'] == 200) {
+				$id = $response['data']['id'];
+				$contact_data = User::findOrFail($id);
+			}
+
         }
 
         /* Add new contact */ 
