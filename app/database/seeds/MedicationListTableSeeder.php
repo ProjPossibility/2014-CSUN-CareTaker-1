bb0809577550ae4614fbe11e20963ee10f7c13a2
Ryan Chua <ryan.chua.15@my.csun.edu>
2014-02-08 20:53:48 -0800

    Added another medication to the master list. Will do commits incrementally as it takes a bit for each new medication information to be entered.

--- app/database/seeds/MedicationListTableSeeder.php
+++ app/database/seeds/MedicationListTableSeeder.php
@@ -39,5 +39,42 @@ class MedicationListTableSeeder extends Seeder {
 				'seizures, severe/persistent headache, severe restlessness, '.
 				'fast breathing.'
 		));
+
+		MedicationList::create(array(
+			'name'			=> '',
+			'dosage'		=> '',
+			'warnings'		=> 'Antidepressant medications are used to treat '.
+				'a variety of conditions, including depression and other '.
+				'mental/mood disorders. These medications can help prevent '.
+				'suicidal thoughts/attempts and provide other important '.
+				'benefits. However, studies have shown that a small number of '.
+				'people (especially people younger than 25) who take '.
+				'antidepressants for any condition may experience worsening '.
+				'depression, other mental/mood symptoms, or suicidal '.
+				'thoughts/attempts. Therefore, it is very important to talk '.
+				'with the doctor about the risks and benefits of antidepressant '.
+				'medication (especially for people younger than 25), even if '.
+				'treatment is not for a mental/mood condition.',
+			'uses'			=> 'Citalopram is used to treat depression. It '.
+				'may improve your energy level and feelings of well-being. '.
+				'Citalopram is known as a selective serotonin reuptake '.
+				'inhibitor (SSRI). This medication works by helping to restore '.
+				'the balance of a certain natural substance (serotonin) in '.
+				'the brain.',
+			'side_effects'	=> 'Nausea, dry mouth, loss of appetite, tiredness, '.
+				'drowsiness, sweating, blurred vision, and yawning',
+			'precautions'	=> 'Before taking citalopram, tell your doctor or '.
+				'pharmacist if you are allergic to it; or to escitalopram; or '.
+				'if you have any other allergies. This product may contain '.
+				'inactive ingredients, which can cause allergic reactions or '.
+				'other problems. Talk to your pharmacist for more details.',
+			'interactions'	=> 'isocarboxazid, linezolid, methylene blue, '.
+				'moclobemide, phenelzine, procarbazine, rasagiline, selegiline, '.
+				'tranylcypromine',
+			'overdose'		=> 'If overdose is suspected, contact a poison '.
+				'control center or emergency room immediately. US residents '.
+				'can call their local poison control center at 1-800-222-1222. '.
+				'Canada residents can call a provincial poison control center.'
+		));
 	}
 }
