1b916c1734b2372dca3cc07aa8c6b761794262ab
WebDevGirl <thedpq@gmail.com>
2014-02-09 14:41:47 -0800

    Add appointment date time to the database.

--- app/database/seeds/AppointmentTableSeeder.php
+++ app/database/seeds/AppointmentTableSeeder.php
@@ -5,11 +5,20 @@ class AppointmentTableSeeder extends Seeder {
     public function run()
     {   
         DB::table('appointments')->delete();
+        
+        $date1 = new DateTime();
+        $date2 = new DateTime();
+        $date3 = new DateTime();
+
+        $date1->modify("+1 day");
+        $date2->modify("+2 day");
+        $date3->modify("+10 day");
 
         Appointment::create(array(
         	'user_id'			=> 1,
             'name'             	=> 'Doctors Appointment',
             'location'        	=> 'UCLA Medical Center',
+            'appointment_datetime' => $date1,
             'created_at'        => new DateTime,
             'updated_at'        => new DateTime
         ));
@@ -18,6 +27,7 @@ class AppointmentTableSeeder extends Seeder {
         	'user_id'			=> 1,
             'name'             	=> 'Pick up grandchildren',
             'location'        	=> 'Wilbur Elementary School',
+            'appointment_datetime' => $date2,
             'created_at'        => new DateTime,
             'updated_at'        => new DateTime
         ));
@@ -26,6 +36,7 @@ class AppointmentTableSeeder extends Seeder {
         	'user_id'			=> 1,
             'name'             	=> 'Haircut',
             'location'        	=> 'Beauty salon',
+            'appointment_datetime' => $date3,
             'created_at'        => new DateTime,
             'updated_at'        => new DateTime
         ));
@@ -33,7 +44,8 @@ class AppointmentTableSeeder extends Seeder {
         Appointment::create(array(
             'user_id'           => 2,
             'name'              => 'Dentist appointment',
-            'location'          => 'All Family Dental Care',
+            'location'          => 'All Family Dental Care',  
+            'appointment_datetime' => $date3,
             'created_at'        => new DateTime,
             'updated_at'        => new DateTime
         ));
@@ -41,6 +53,7 @@ class AppointmentTableSeeder extends Seeder {
         Appointment::create(array(
             'user_id'           => 3,
             'name'              => 'Pick up Dr Pepper',
+            'appointment_datetime' => $date1,
             'location'          => 'Ralphs',
             'created_at'        => new DateTime,
             'updated_at'        => new DateTime
@@ -50,6 +63,7 @@ class AppointmentTableSeeder extends Seeder {
             'user_id'           => 4,
             'name'              => 'Rock climbing',
             'location'          => 'Top Out Climbing Center',
+            'appointment_datetime' => $date2,
             'created_at'        => new DateTime,
             'updated_at'        => new DateTime
         ));
@@ -58,6 +72,7 @@ class AppointmentTableSeeder extends Seeder {
             'user_id'           => 5,
             'name'              => 'Study session',
             'location'          => 'CSUN Oviatt Library',
+            'appointment_datetime' => $date3,
             'created_at'        => new DateTime,
             'updated_at'        => new DateTime
         ));
@@ -65,6 +80,7 @@ class AppointmentTableSeeder extends Seeder {
         Appointment::create(array(
             'user_id'           => 6,
             'name'              => 'Mentor students',
+            'appointment_datetime' => $date1,
             'location'          => 'CSUN',
             'created_at'        => new DateTime,
             'updated_at'        => new DateTime
