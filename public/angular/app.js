f4fa74a0ef3df5792e615ca26e69ac9f9bcee7a1
Ryan Chua <ryan.chua.15@my.csun.edu>
2014-02-08 17:58:39 -0800

    Added angular routes for adding and modifying a medication. Added an update method in the medications controller. Added pluck and update requests to the medication factory. Added new partial views for adding a new medication and for modifying an existing medication record.

--- public/angular/app.js
+++ public/angular/app.js
@@ -19,6 +19,14 @@ var app = angular
 					templateUrl: 'angular/partials/home.html',
 					controller: 'HomeCtrl'
 				})
+				.when('/add-medication', {
+					templateUrl: 'angular/partials/add_medication.html',
+					controller: 'MedicationsCtrl'
+				})
+				.when('/medications/:medication_id/modify', {
+					templateUrl: 'angular/partials/modify_medication.html',
+					controller: 'MedicationsCtrl'
+				})
 				.otherwise(({redirectTo: '/'}))
 		}
 	])
