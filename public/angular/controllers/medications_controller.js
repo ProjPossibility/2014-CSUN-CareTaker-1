ea3b5bdfcacce63c51686cee44d81a6cde18f2bd
Ryan Chua <ryan.chua.15@my.csun.edu>
2014-02-09 15:54:51 -0800

    Added a delete method for removing a medication. Added comments. Changed Event Date to Appointment Date when creating a new appointment. Table of medications will be hidden when adding a new medication. Added delete API call to the medications controller. Added a remove button when modifying a medication.

--- public/angular/controllers/medications_controller.js
+++ public/angular/controllers/medications_controller.js
@@ -1,11 +1,13 @@
 app.controller("MedicationsCtrl", function ($scope, $rootScope, $routeParams, Medications) {
 
+	// Grab all medications for the current user
 	Medications.get({},
 		function(data) {
 			$scope.medications = data.data;
 		}
 	)
 
+	// Call pluck only when the medication_id is present in the route
 	if ($routeParams.medication_id) {
 		Medications.pluck({
 			id: $routeParams.medication_id
@@ -14,6 +16,7 @@ app.controller("MedicationsCtrl", function ($scope, $rootScope, $routeParams, Me
 		})
 	}
 
+	// Add a new medication for the user
 	$scope.addMed = function(medication) {
 		Medications.add({
 			user_id: $rootScope.user_id,
@@ -26,6 +29,7 @@ app.controller("MedicationsCtrl", function ($scope, $rootScope, $routeParams, Me
 		})
 	}
 
+	// Update specified medication's information
 	$scope.updateMed = function(medication) {
 		Medications.update({
 			id: $routeParams.medication_id,
@@ -34,6 +38,15 @@ app.controller("MedicationsCtrl", function ($scope, $rootScope, $routeParams, Me
 			requirements: medication.requirements,
 			notes: medication.notes
 		}, function (data) {
+			window.location = "#/medications"
+		})
+	}
+
+	// Delete specified medication from user's list.
+	$scope.deleteMed = function(medication_id) {
+		Medications.delete({
+			id: medication_id
+		}, function(data) {
 			window.location = "#/medications"
 		})
 	}
