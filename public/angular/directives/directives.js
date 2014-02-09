415dab66bfb8091463014624fb270ed563d6ff2e
Arvin Foroutan <arvin.foroutan@gmail.com>
2014-02-09 12:58:11 -0800

    Created Notifications factory. Created directive for Reminder todos and added it to the main page

--- public/angular/directives/directives.js
+++ public/angular/directives/directives.js
@@ -66,3 +66,50 @@ angular.module('directives', [])
 		controller: 'CalendarCtrl'
 	}
 })
+.directive('reminder', function() {
+	return {
+		restrict: 'E',
+		replace: true,
+		controller: function($scope, $rootScope, Notifications) {
+
+			$scope.toggleReminder = function(reminder)
+			{
+				Notifications.put({
+					'id': reminder.id
+				}, function (data) {
+					//
+				});
+			}
+
+			$scope.remaining = function()
+			{
+				var count = 0;
+				
+				angular.forEach($scope.notifications, function (reminder) {
+			  		count += reminder.done == 1 ? 0 : 1;
+				});
+				
+				return count;
+			};
+
+			$scope.archive = function()
+			{
+				var oldTodos = $scope.notifications;
+				
+				$scope.notifications = [];
+				
+				angular.forEach(oldTodos, function (reminder) {
+			  		if (reminder.done == 0) $scope.notifications.push(reminder);
+			  		else {
+			  			Notifications.delete({
+			  				'id': reminder.id
+			  			}, function (data) {
+			  				//
+			  			});
+			  		}
+				});
+			};
+		},
+		templateUrl: 'angular/directives/reminders.html'
+	}
+})
