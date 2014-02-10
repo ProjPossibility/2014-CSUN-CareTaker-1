acf96dff347a239c45423e43e989ed7507aff250
Arvin Foroutan <arvin.foroutan@gmail.com>
2014-02-09 18:34:11 -0800

    Linked up Android with the API to receive notifications

--- android/src/com/csun2014css12/carefree/CareService.java
+++ android/src/com/csun2014css12/carefree/CareService.java
@@ -1,20 +1,55 @@
 package com.csun2014css12.carefree;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class CareService extends IntentService {
+	
+    public static final String LOG_TAG = "CareService";
 
-	public CareService() {
-		super("CareService");
+    public CareService() {
+        super("CareService");
+    }
+    
+    @Override
+    public void onCreate(){
+    	super.onCreate();
     }
 
-	@Override
+    @Override
     protected void onHandleIntent(Intent workIntent) {
-        // Gets data from the incoming Intent
-        String dataString = workIntent.getDataString();
-        
-        // Do work here, based on the contents of dataString
-
+    	try {
+    		HttpClient httpclient = new DefaultHttpClient();
+    		HttpGet httpget = new HttpGet("http://carefree.v4.net/carefree/public/api/v1/users/1/generate-notifications");
+    		HttpResponse response = httpclient.execute(httpget);
+    		
+    		BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "iso-8859-1"), 8);
+    		StringBuilder sb = new StringBuilder();
+    		sb.append(reader.readLine() + "\n");
+    		String line = "0";
+           
+    		while ((line = reader.readLine()) != null) {
+    			sb.append(line + "\n");
+    		}
+    		reader.close();
+    		String results = sb.toString();
+             
+    		if (results != null && results != "") {
+    			 Log.d("serviceRESULTS",results);
+    		} else {
+        	   Log.d("serviceRESULTS","none");
+    		}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
     }
 }
--- android/src/com/csun2014css12/carefree/MainActivity.java
+++ android/src/com/csun2014css12/carefree/MainActivity.java
@@ -1,16 +1,78 @@
 package com.csun2014css12.carefree;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private Intent mServiceIntent;
+	public final static String EXTRA_MESSAGE = "com.csun2014css12.carefree.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+        Button btn = (Button) findViewById(R.id.get_notifications);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	viewNotifications();
+            }
+        });
 	}
+	
+	protected void viewNotifications() {
+		new GetNotifications().execute();
+	}
+	
+	public class GetNotifications extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpGet httpget = new HttpGet("http://carefree.v4.net/carefree/public/api/v1/users/7/generate-notifications");
+                HttpResponse response = httpclient.execute(httpget);
+                
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                
+                sb.append(reader.readLine() + "\n");
+                String line = "0";
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                reader.close();
+                String result = sb.toString();
+
+                return new String(result);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Intent notificationsIntent = new Intent(getApplicationContext(), NotificationsActivity.class);
+            notificationsIntent.putExtra(EXTRA_MESSAGE, result);
+            startActivity(notificationsIntent);
+        }
+    }
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
